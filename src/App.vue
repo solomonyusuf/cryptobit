97f5c777920939c248f748d75da3a07123cc9db6
Solomon yusuf <olafire03@gmail.com>
2025-01-21 17:23:45 +0100

    added google icon and made heading bolder

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,26 @@
           <div class="bit-row bit-justify-content-center">
             <div class="bit-card bit-col-md-7 bit-col-sm-12">
                 <div class="bit-card-body">
-                  <h2 class="text-widget bit-mt-4">Start Your Journey With a Secured Trading Ways!</h2>
+                  <h2 class="text-widget bit-mt-4 fw-bold">Start Your Journey With a Secured Trading Ways!</h2>
                   <h4 style="color: #a39c9c;" class="text-widget">Enter the information to know more </h4>
                   <img class="image-widget" src="/images/bg1.png"  />
                 </div>
             </div>
             <div class="bit-col-md-5 bit-col-sm-12">
                 <div class="bit-row bit-justify-content-center">
-                  <div class="container-xxl bit-mt-4 bit-mb-4">
+                  <div class=" bit-px-4 bit-mt-4 bit-mb-4">
                       
-                       <form class="bit-gap-2 bit-mt-4 bit-px-4">
-                        <h3 class="text-widget">Login to Access </h3>
+                       <form class="bit-mt-4">
+                        <h3 class="text-widget fw-bold">Login to Access </h3>
                        <p>Don't have an account? <a>sign up here</a></p>
                        <div class="bit-mb-3">
-
+                        <button type="button" class="bit-btn btn-widget bit-w-100 d-flex bit-gap-2">
+                          <img src="/images/google.png" class="button-image rounded-pill" />
+                          Get started with Google
+                        </button>
                        </div>
                        <div class="divider bit-mb-3">
-                        <div class="divider-text">Or Sign up through email</div>
+                        <div class="divider-text">Or Sign up via email</div>
                          </div>
                         <div class="bit-mb-3">
                           <input type="text" class="bit-form-control" placeholder="Enter Your Email">
@@ -67,12 +70,16 @@
   .text-widget{
     color:white;
   }
-
+  .button-image{
+    height:25px;
+  }
   .image-widget{
     flex: 0 0 auto;
     width: 76.66666667%;
   }
-
+  .d-flex {
+  display: flex !important;
+}
 </style>
 
  
