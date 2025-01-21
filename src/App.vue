77118749e1b977f924e2be344e4bf0dbd522bd07
Solomon yusuf <olafire03@gmail.com>
2025-01-21 16:05:11 +0100

    adjusted the responsiveness, added a divider

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,21 @@
         <div class="container-xxl bit-mt-4 bit-mb-4">
           
           <div class="bit-row bit-justify-content-center">
-            <div class="bit-card bit-col-md-7">
+            <div class="bit-card bit-col-md-7 bit-col-sm-12">
                 <div class="bit-card-body">
                   <h2 class="text-widget bit-mt-4">Start Your Journey With a Secured Trading Ways!</h2>
                   <h4 style="color: #a39c9c;" class="text-widget">Enter the information to know more </h4>
                   <img class="image-widget" src="/images/bg1.png"  />
                 </div>
             </div>
-            <div class="bit-col-md-5">
+            <div class="bit-col-md-5 bit-col-sm-12">
                 <div class="bit-row bit-justify-content-center">
                   <div class="container-xxl bit-mt-4 bit-mb-4">
                       <h3 class="text-widget">Login to Access </h3>
                        <p>Don't have an account? <a>sign up here</a></p>
-
+                       <div class="divider">
+                        <div class="divider-text">Or Sign up through email</div>
+                      </div>
                        <form class="bit-gap-2">
                         <div class="bit-mb-3">
                           <input type="text" class="bit-form-control" placeholder="Enter Your Email">
