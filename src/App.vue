451144286a09f2ad37baa912aac479ae2bb3bd1f
Solomon yusuf <olafire03@gmail.com>
2025-01-21 15:45:35 +0100

    worked and designed the inputs

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
    <div class="content-wrapper">
         <div class="container-xxl bit-mt-4 bit-mb-4">
           
-          <div class="bit-row">
+          <div class="bit-row bit-justify-content-center">
             <div class="bit-card bit-col-md-7">
                 <div class="bit-card-body">
                   <h2 class="text-widget bit-mt-4">Start Your Journey With a Secured Trading Ways!</h2>
@@ -17,7 +17,30 @@
                 </div>
             </div>
             <div class="bit-col-md-5">
+                <div class="bit-row bit-justify-content-center">
+                  <div class="container-xxl bit-mt-4 bit-mb-4">
+                      <h3 class="text-widget">Login to Access </h3>
+                       <p>Don't have an account? <a>sign up here</a></p>
 
+                       <form class="bit-gap-2">
+                        <div class="bit-mb-3">
+                          <input type="text" class="bit-form-control" placeholder="Enter Your Email">
+                        </div>
+                        <div class="bit-mb-3">
+                          <input type="password" class="bit-form-control" placeholder="Enter Your Password">
+                        </div>
+                        <div class="bit-mb-3">
+                          <div class="bit-form-check">
+                            <input class="bit-form-check-input" type="checkbox" value="" id="defaultCheck3" checked="">
+                            <label class="bit-form-check-label"> I agree to the Terms and Conditions </label>
+                          </div>
+                        </div>
+                        <div class="bit-mb-3">
+                          <button type="button" class="bit-btn bit-btn-primary bit-w-100">Continue</button>
+                        </div>
+                       </form>
+                   </div>
+                </div>
             </div>
           </div>
 
