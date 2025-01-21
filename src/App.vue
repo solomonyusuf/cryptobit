e589e0d284fd1dd9de0c07595c85283a140ffc51
Solomon yusuf <olafire03@gmail.com>
2025-01-21 07:42:22 +0100

    adjusted the feft panel

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,14 @@
         <div class="container-xxl bit-mt-4 bit-mb-4">
           
           <div class="bit-row">
-            <div class="bit-card bit-col-md-8">
+            <div class="bit-card bit-col-md-7">
                 <div class="bit-card-body">
                   <h1 class="text-widget bit-mt-4">Start Your Journey With a Secured Trading Ways!</h1>
                   <h4 style="color: #a39c9c;" class="text-widget">Enter the information to know more </h4>
                   <img class="image-widget" src="/images/bg1.png"  />
                 </div>
             </div>
-            <div class="bit-col-md-4">
+            <div class="bit-col-md-5">
 
             </div>
           </div>
