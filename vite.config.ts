fee49eca8dac526a56e6d35911f4ba53eacf380d
Solomon yusuf <olafire03@gmail.com>
2025-02-12 18:00:27 +0100

    configure vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-vue';
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(),
+        vercel()
+    ],
     server: {
         port: 51308,
     }
