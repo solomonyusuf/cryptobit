1f1a8b9dd851803379808107f143b95591db7374
Solomon yusuf <olafire03@gmail.com>
2025-02-12 18:16:09 +0100

    deploy to render

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 import Login from './components/pages/Login.vue';
 import ListView from './components/pages/ListView.vue';
 import ChartView from './components/pages/ChartView.vue';
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // Use hash mode
   routes: [
-    
-    {
-      path: '/chart',
-      name: 'Pair',
-      component: ChartView,
-    },
-    {
-      path: '/',
-      name: 'List',
-      component: ListView,
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-    },
+    { path: '/', name: 'List', component: ListView },
+    { path: '/chart', name: 'Pair', component: ChartView },
+    { path: '/login', name: 'Login', component: Login },
   ],
 });
+
 
 createApp(App).use(router).mount('#app')
