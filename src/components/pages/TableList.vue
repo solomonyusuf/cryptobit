88e63274a38604000dbdbbddf8e09335895c0b92
Solomon yusuf <olafire03@gmail.com>
2025-02-15 15:44:09 +0100

    adjust font

--- src/components/pages/TableList.vue
+++ src/components/pages/TableList.vue
@@ -1751,7 +1751,7 @@ body
 
 body {
   margin: 0;
-  font-family:  -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, Ubuntu, 'Helvetica Neue', sans-serif;
+  font-family:  var(--font-medium-medium);
   font-size:13px;
   font-weight: var(--bs-body-font-weight);
   line-height: var(--bs-body-line-height);
@@ -1761,7 +1761,52 @@ body {
   -webkit-text-size-adjust: 100%;
   -webkit-tap-highlight-color: rgba(34, 48, 62, 0);
 }
-
+:root {
+  --fontFamily-base: "Satoshi", system-ui, -apple-system, Helvetica, Arial, sans-serif;
+  --fontFamily-monospace: Courier, monospace, var(--fontFamily-base);
+  --fontWeight-base-0: 450;
+  --fontWeight-base-1: 500;
+  --fontWeight-base-2: 700;
+  --letter-spacing-base: -.02em;
+  --fontSize-tiny: .625rem;
+  --fontSize-mini: .75rem;
+  --fontSize-small: .8438rem;
+  --fontSize-base: .9375rem;
+  --fontSize-medium: 1.125rem;
+  --fontSize-large: 1.375rem;
+  --fontSize-extra: 1.75rem;
+  --fontWeight-regular: 400;
+  --fontWeight-book: 450;
+  --fontWeight-medium: 500;
+  --fontWeight-semibold: 600;
+  --fontWeight-bold: 700;
+  --font-tiny-regular: var(--fontWeight-regular) var(--fontSize-tiny) var(--fontFamily-base);
+  --font-tiny-book: var(--fontWeight-book) var(--fontSize-tiny) var(--fontFamily-base);
+  --font-tiny-medium: var(--fontWeight-medium) var(--fontSize-tiny) var(--fontFamily-base);
+  --font-mini-regular: var(--fontWeight-regular) var(--fontSize-mini) var(--fontFamily-base);
+  --font-mini-book: var(--fontWeight-book) var(--fontSize-mini) var(--fontFamily-base);
+  --font-mini-medium: var(--fontWeight-medium) var(--fontSize-mini) var(--fontFamily-base);
+  --font-small-regular: var(--fontWeight-regular) var(--fontSize-small) var(--fontFamily-base);
+  --font-small-book: var(--fontWeight-book) var(--fontSize-small) var(--fontFamily-base);
+  --font-small-medium: var(--fontWeight-medium) var(--fontSize-small) var(--fontFamily-base);
+  --font-small-bold: var(--fontWeight-bold) var(--fontSize-small) var(--fontFamily-base);
+  --font-base-regular: var(--fontWeight-regular) var(--fontSize-base) var(--fontFamily-base);
+  --font-base-book: var(--fontWeight-book) var(--fontSize-base) var(--fontFamily-base);
+  --font-base-medium: var(--fontWeight-medium) var(--fontSize-base) var(--fontFamily-base);
+  --font-base-bold: var(--fontWeight-bold) var(--fontSize-base) var(--fontFamily-base);
+  --font-medium-regular: var(--fontWeight-regular) var(--fontSize-medium) var(--fontFamily-base);
+  --font-medium-book: var(--fontWeight-book) var(--fontSize-medium) var(--fontFamily-base);
+  --font-medium-medium: var(--fontWeight-medium) var(--fontSize-medium) var(--fontFamily-base);
+  --font-medium-bold: var(--fontWeight-bold) var(--fontSize-medium) var(--fontFamily-base);
+  --font-large-regular: var(--fontWeight-regular) var(--fontSize-large) var(--fontFamily-base);
+  --font-large-book: var(--fontWeight-book) var(--fontSize-large) var(--fontFamily-base);
+  --font-large-medium: var(--fontWeight-medium) var(--fontSize-large) var(--fontFamily-base);
+  --font-extra-regular: var(--fontWeight-regular) var(--fontSize-extra) var(--fontFamily-base);
+  --font-extra-book: var(--fontWeight-book) var(--fontSize-extra) var(--fontFamily-base);
+  --font-extra-medium: var(--fontWeight-medium) var(--fontSize-extra) var(--fontFamily-base);
+  --font-extra-bold: var(--fontWeight-bold) var(--fontSize-extra) var(--fontFamily-base);
+  --fontFeature-monoNumbers: "tnum" on, "lnum" on, "zero" 1;
+}
 hr {
   margin: 1rem 0;
   color: #e4e6e8;
